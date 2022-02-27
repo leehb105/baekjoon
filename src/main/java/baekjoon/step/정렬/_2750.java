7e87d7824564be9fead321fc1cee6135f993e2bc
leehb105 <leesonge95@gmail.com>
2022-02-28 05:10:29 +0900

    답안 추가

---
+++ src/main/java/baekjoon/step/정렬/_2750.java
@@ -0,0 +1,29 @@
+package baekjoon.step.정렬;
+
+import java.util.Scanner;
+
+public class _2750 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner((System.in));
+		int size = sc.nextInt();
+		int []arr = new int[size];
+	
+		for(int i = 0; i < arr.length; i++) {//숫자 입력 
+			arr[i] = sc.nextInt();
+		}
+		int temp = 0;
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i+1; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i = 0; i < arr.length; i++) {//숫자 입력 
+			System.out.println(arr[i]);
+		}
+		
+	}
+}
