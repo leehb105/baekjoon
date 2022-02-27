fce6db40932351dee3542574a7ff8c1e578fb5b7
leehb105 <leesonge95@gmail.com>
2022-02-28 05:09:44 +0900

    답안 추가

---
+++ src/main/java/baekjoon/step/재귀/_10870.java
@@ -0,0 +1,21 @@
+package baekjoon.step.재귀;
+
+import java.util.Scanner;
+
+public class _10870 {
+	public static int calculate(int num) {
+		int result = 0;
+		if(num <= 1) {
+			return num;
+		}else {
+			result = calculate(num -1) + calculate(num -2);
+			return result;
+		}
+	}
+	public static void main(String[] args) {
+		int inputNum;
+		Scanner sc = new Scanner(System.in);
+		inputNum = sc.nextInt();		
+		System.out.println(calculate(inputNum));
+    }
+}
