3cfe40b041462552a5f069a4fe9e9d9cb904eab2
leehb105 <leesonge95@gmail.com>
2022-05-29 17:32:19 +0900

    counting정렬 구현

--- src/main/java/baekjoon/sorting/_10989.java
+++ src/main/java/baekjoon/sorting/_10989.java
@@ -1,29 +1,39 @@
 package baekjoon.sorting;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class _10989 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		int[] arr = new int[input];
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.valueOf(br.readLine());
+		int[] counting = new int[10001];
+		int[] arr = new int[n];
+		
+		//입력값 counting배열에 갯수 지정
+		for(int i = 0; i < n; i++) {
+			int input = Integer.parseInt(br.readLine());
+			counting[input]++;
 		}
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = i + 1; j < arr.length; j++) {
-				if(arr[i] > arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+		for(int i = 1; i < counting.length; i++) {
+			while(counting[i] > 0) {
+				sb.append(i).append("\n");
+				counting[i]--;
+				
 			}
 		}
-		for(int a : arr) {
-			System.out.println(a);
-		}
 
+		bw.write(sb.toString());
+		bw.flush();
+        bw.close();
+        br.close();
 	}
 
 }
